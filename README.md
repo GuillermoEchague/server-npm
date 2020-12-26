2d04e231f21dc7addf1d68cfa73c0d6e8403ab0c
Guillermo Echague <guillermo.echaguea@gmail.com>
2020-12-26 02:56:14 -0300

    proyecto web server npm

---
+++ README.md
@@ -0,0 +1,43 @@
+
+
+## Inicio proyecto en Node.js
+
+```
+npm init -y
+```
+
+crear app.js
+
+
+## Instalar Express
+
+```
+pm i express --save
+```
+
+## Creae HTML
+
+(!+Tab) con snipper
+
+
+## Middleware
+app.use(express.static(__dirname + '/public'));
+
+
+## Boostrap
+
+getbootstrap.com
+
+dist/
+
+## Handlebars
+
+```
+npm install hbs --save
+```
+
+## Correr servidor server
+
+```
+nodemon server -e js,hbs,html,css
+```
---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+ 
+const hbs = require('hbs');
+
+// helpers
+require('./hbs/helpers');
+
+
+//middleware
+app.use(express.static(__dirname + '/public'));
+
+// Express HBS engine
+hbs.registerPartials( __dirname+'/views/partials');
+app.set('view engine', 'hbs');
+
+
+
+
+
+app.get('/', (req, res)=> {
+
+  res.render('home',{
+    nombre: 'Guillermo',
+});
+});
+
+
+app.get('/about', (req, res)=> {
+
+    res.render('about',{
+});
+});
+  
+
+
+app.listen(3000, ()=> {
+    console.log('Escuchando peticiones en el puerto 3000');
+});
