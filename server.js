4cd753302b03f25f9a12003a921ef76cb5e72114
Guillermo Echague <guillermo.echaguea@gmail.com>
2020-12-26 03:15:38 -0300

    Cambio de nombre de public correccion path

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require('./hbs/helpers');
 const port = process.env.PORT || 3000;
 
 //middleware
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/docs'));
 
 // Express HBS engine
 hbs.registerPartials( __dirname+'/views/partials');
